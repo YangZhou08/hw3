b635e23d0e0cc101e2af00cf957d2f8e8e36bf92
Yang Zhou <yangzhou@Yangs-MacBook-Pro-6.local>
2024-10-31 17:56:00 -0400

    running

--- src/ndarray_backend_cpu.cc
+++ src/ndarray_backend_cpu.cc
@@ -155,7 +155,36 @@ void ScalarSetitem(const size_t size, scalar_t val, AlignedArray* out, std::vect
    */
 
   /// BEGIN SOLUTION
-  assert(false && "Not Implemented");
+  int shapesize = shape.size(); 
+  std::vector<int32_t> indices(shapesize, 0); 
+  int cnt = 0; 
+
+  while (true) { 
+    // compute the flattened index 
+    size_t flat_index = 0; 
+    for (int i = 0; i < indices.size(); i++) { 
+      flat_index += indices[i] * strides[i]; 
+    } 
+
+    indices[shapesize - 1]++; 
+
+    // out[cnt] = a.ptr[offset + flat_index]; 
+    // out.ptr[offset + flat_index] = a[cnt]; 
+    out->ptr[offset + flat_index] = a.ptr[cnt]; 
+    cnt++; 
+
+    // carry 
+    for (int i = shapesize - 1; i >= 0; i--) { 
+      if (indices[i] == shape[i]) { 
+        if (i == 0) { 
+          return; 
+        } 
+        indices[i] = 0; 
+        indices[i - 1]++; 
+      } 
+    } 
+
+  } 
   /// END SOLUTION
 }
 
